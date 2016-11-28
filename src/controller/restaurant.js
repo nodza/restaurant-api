ff750b6937ff31b541ddeaf6ad867ec02fbbe16a
nodza <noel.hwande@gmail.com>
2016-11-27 20:03:12 -0600

    Added PUT method and route

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -36,8 +36,24 @@ export default({ config, db }) => {
                 res.send(err);
             }
             res.json(restaurant);
-        })
-    })
+        });
+    });
+
+    // 'v1/restaurant/:id'
+    api.put('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            restaurant.name = req.body.name;
+            restaurant.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: 'Restaurant updated successfully '});
+            });
+        });
+    });
 
     return api;
 }
